18d02751f3cacf3e9afeeb0c45cc6ad1d0105723
ke.cao <ke.cao@17zuoye.com>
2018-05-23 10:45:36 +0800

    文档

--- src/components/servers/file.js
+++
@@ -1,9 +0,0 @@
-//往服务容器 添加服务
-/*api
- Bus.$emit("getFile",function (file) {
-    console.log(file)
-  })
-* */
-import Bus from '../bus';
-Bus.addModalComponent(require('./file.vue'))
-export default Bus;
--- src/components/servers/pswp.js
+++
@@ -1,8 +0,0 @@
-//注册服务
-/*api
-
- Bus.$emit("mvGallery",index,lists)
-* */
-import Bus from '../bus';
-Bus.addModalComponent(require('./pswp.vue'))
-export default Bus;
