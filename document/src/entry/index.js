cf96b8242141d711f1de4d3ec71bf889ce36cd40
ke.cao <ke.cao@17zuoye.com>
2018-06-01 19:28:48 +0800

    ad

--- document/src/entry/index.js
+++ document/src/entry/index.js
@@ -14,7 +14,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI, { size: 'small' });
 
 //项目的入口
-import App from '../views/index'
+import App from '../views/app'
 
 /* eslint-disable no-new */
 new Vue({
---
+++ document/src/views/app.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app">
+    <div class="top">
+      <el-menu
+
+        :default-active="'1'"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="index">17作业组件文档平台</el-menu-item>
+        <el-menu-item index="marvel">marvel组件</el-menu-item>
+        <el-menu-item index="page2">源码下载</el-menu-item>
+      </el-menu>
+    </div>
+    <div :key="$router.path">
+      <router-view/>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'App',
+    data () {
+      return {
+        page1:{},
+        page2:{
+          info1:{}
+        },
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        if(key){
+          this.$router.push({ name: key})
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .app{
+    padding-top: 61px;
+  }
+  .top{
+    position: fixed;
+    width: 100%;
+    top: 0;
+    height: 0;
+    z-index: 11;
+  }
+
+</style>
