8545a6e5a52e007dc9991aaaf470f11f9e936587
ke.cao <ke.cao@17zuoye.com>
2018-06-01 19:21:51 +0800

    add

---
+++ document/src/common/pcbase.js
@@ -0,0 +1,18 @@
+//pc端：使用路由、图片懒加载、element-ui、服务、组件
+import Vue from 'vue'
+
+/*
+* 图片懒加载*/
+Vue.component('mv-img', require('../marvel/img.vue'));
+/*
+  1、注册 服务容器 mv-modal
+  增加服务的接口
+* */
+
+Vue.component('mv-modal', require('../marvel/modal.vue'));
+
+
+//项目公用的
+import '../common/pcbase.css';
+
+export default Vue;
