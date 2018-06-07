681e8801fc7051dd73ab71c67aa338472892eab8
ke.cao <ke.cao@17zuoye.com>
2018-06-07 19:03:56 +0800

    dev

--- document/build/webpack.base.conf.js
+++ document/build/webpack.base.conf.js
@@ -38,6 +38,11 @@ module.exports = {
         options: vueLoaderConfig
       },
       {
+        test: /\.md$/,
+        loader: 'text-loader',
+        include: [resolve('src')]
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         options: {
