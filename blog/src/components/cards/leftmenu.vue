29b8e552a9ffeee6b0f722dc239203c01c6d3eb9
ke.cao <ke.cao@17zuoye.com>
2018-11-23 10:52:57 +0800

    blog

--- blog/src/components/cards/leftmenu.vue
+++ blog/src/components/cards/leftmenu.vue
@@ -5,16 +5,31 @@
       @select="handleSelect">
       <template v-for="(v,k) in card.submenu" >
         <el-submenu v-if="v.submenu" :index="v.name||v.path">
-          <template slot="title"><span @click="mclick(v)">{{v.title}}</span></template>
+          <template slot="title">
+            <i v-if="v.icon" :class="v.icon"></i>
+            <span @click="mclick(v)">{{v.title}}</span>
+          </template>
           <template v-for="(v2,k2) in v.submenu" >
             <el-submenu v-if="v2.submenu" :index="v2.name||v2.path">
-              <template slot="title"><div @click="mclick(v2)">{{v2.title}}</div></template>
-              <el-menu-item :index="v3.name||v3.path" v-for="(v3,k3) in v2.submenu" :key="k3"><div @click="mclick(v3)">{{v3.title}}</div></el-menu-item>
+              <template slot="title">
+                <i v-if="v2.icon" :class="v2.icon"></i>
+                <div @click="mclick(v2)">{{v2.title}}</div>
+              </template>
+              <el-menu-item :index="v3.name||v3.path" v-for="(v3,k3) in v2.submenu" :key="k3">
+                <i v-if="v3.icon" :class="v3.icon"></i>
+                <div @click="mclick(v3)">{{v3.title}}</div>
+              </el-menu-item>
             </el-submenu>
-            <el-menu-item v-else :index="v2.name||v2.path"><span @click="mclick(v2)">{{v2.title}}</span></el-menu-item>
+            <el-menu-item v-else :index="v2.name||v2.path">
+              <i v-if="v2.icon" :class="v2.icon"></i>
+              <span @click="mclick(v2)">{{v2.title}}</span>
+            </el-menu-item>
           </template>
         </el-submenu>
-        <el-menu-item v-else :index="v.name||v.path"><span @click="mclick(v)">{{v.title}}</span></el-menu-item>
+        <el-menu-item v-else :index="v.name||v.path">
+          <i v-if="v.icon" :class="v.icon"></i>
+          <span @click="mclick(v)">{{v.title}}</span>
+        </el-menu-item>
       </template>
     </el-menu>
     <div class="line"></div>
